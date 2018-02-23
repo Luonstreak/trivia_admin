d8ce1a5cf913c8ae29271351f7a845afdd1a2207
Lucio Rodriguez <luonstreak@gmail.com>
2018-02-23 15:50:18 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import firebase from 'firebase';
+import { StyleSheet } from 'react-native';
+import Control from './Control';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDqYuLYEHSeiO0PrT4zPWornW7mHX8uP80",
+      authDomain: "trivia-1ec67.firebaseapp.com",
+      databaseURL: "https://trivia-1ec67.firebaseio.com",
+      projectId: "trivia-1ec67",
+      storageBucket: "trivia-1ec67.appspot.com",
+      messagingSenderId: "308224065203"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render(){
+    return (
+      <Control />
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50
+  },
+});
---
+++ Control.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button } from 'react-native-elements';
+// import LinearGradient from 'react-native-linear-gradient';
+import firebase from 'firebase';
+
+class Control extends Component {
+
+    sendOrder = (type) => {
+        const ref = firebase.database().ref('socket')
+        var order;
+        switch (type) {
+            case 'init':
+                ref.push({
+                    order: 'game_init',
+                    sender: 'admin'
+                });
+                break;
+            case 'quest':
+                ref.push({
+                    order: 'quest',
+                    sender: 'admin'
+                });
+                break;
+            case 'expl':
+                ref.push({
+                    order: 'expl',
+                    sender: 'admin'
+                });
+                break;
+            default:
+                break;
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    buttonStyle={{ marginBottom: 20, width: 200 }}
+                    title="START GAME"
+                    onPress={() => this.sendOrder('init')}
+                />
+                <Button
+                    buttonStyle={{ marginBottom: 20, width: 200 }}
+                    title="RENDER QUESTION"
+                    onPress={() => this.sendOrder('quest')}
+                />
+                <Button
+                    buttonStyle={{ marginBottom: 20, width: 200 }}
+                    title="RENDER EXPLANATION"
+                    onPress={() => this.sendOrder('expl')}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 50
+    },
+    linearGradient: {
+        borderRadius: 50,
+        marginBottom: 20,
+        width: 200
+    },
+    buttonText: {
+        backgroundColor: 'transparent'
+    }
+});
+
+export default Control;
